51910529dff685291cd398242b08a27fa18d7fcd
capi <cpinelly@gmail.com>
2024-06-12 19:53:49 -0500

    Basic balance view.

--- composeApp/src/desktopMain/kotlin/App.kt
+++ composeApp/src/desktopMain/kotlin/App.kt
@@ -1,13 +1,17 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -15,6 +19,22 @@ import moneytracker.composeapp.generated.resources.Res
 import moneytracker.composeapp.generated.resources.compose_multiplatform
 import java.io.File
 
+@Composable
+fun AccountBalance(accountName: String, balance: String) {
+    Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween) {
+        Text(accountName)
+        Text(balance)
+    }
+}
+@Composable
+fun Balance() {
+    Column(modifier = Modifier.width(300.dp)) {
+        AccountBalance("Total", "$2.000,21")
+        AccountBalance("Total", "$2.000,21")
+        AccountBalance("Total", "$2.000,21")
+    }
+}
 @Composable
 @Preview
 fun App() {
@@ -24,22 +44,9 @@ fun App() {
     val mt = MoneyTrackerCLI(scriptLocation, "sqlite::test.db")
 
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+//        var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                showContent = !showContent
-                val result = mt.accounts.create("From kotlin")
-                println(result)
-            }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            Balance()
         }
     }
 }
